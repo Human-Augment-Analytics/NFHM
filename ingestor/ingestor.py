37ef92dace036b28dcb469ba209fd2e227389994
Shimer, Jacob Daniel <jacob.d.shimer@gmail.com>
2024-05-22 17:57:36 -0400

    Setup devcontainers to be separated between backend and ingestor Started working on settings management for the ingestor

--- ingestor/ingestor.py
+++ ingestor/ingestor.py
@@ -1,14 +1,17 @@
 import asyncio
 import logging
 import signal
-
-from inputs.gbif import gbif_search
-# from outputs.json_output import dump_to_json
-from outputs.mongo_output import dump_to_mongo
-from ingest_queue import RedisQueue
-from worker import RedisWorker
+from typing import Any, Callable, Type
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase, AsyncIOMotorCollection
+from pydantic import BaseModel, Field, ImportString, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from inputs import gbif_search
+# from outputs.json_output import dump_to_json
+from outputs import dump_to_mongo
+from ingest_queue import RedisQueue, BaseQueue
+from worker import RedisWorker
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -20,20 +23,77 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+class RedisSettings(BaseModel):
+    host: str
+    port: int = Field(default=6379)
+    username: str | None = None
+    password: str | None = None
+
+
+class MongoSettings(BaseModel):
+    host: str
+    port: int = Field(default=27017)
+    database: str = Field(default='NFHM')
+    username: str | None = None
+    password: str | None = None
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_nested_delimiter='__')
+    source_queue: str
+    redis: RedisSettings | None = None
+    mongo: MongoSettings | None = None
+    queue: ImportString[Type[BaseQueue]] = Field(default='ingest_queue.RedisQueue')
+    input: ImportString[Callable[[Any], Any]] = Field(default='inputs.gbif_search')
+    output: ImportString[Callable[[Any], Any]] = Field(default='outputs.dump_to_mongo')
+
+    @model_validator(mode='after')
+    @classmethod
+    def validate_queue_settings(cls, data: Any) -> Any:
+        if isinstance(data, dict):
+            if data.get('queue').rsplit('.')[-1] == 'RedisQueue':
+                assert data.get('redis') is not None, 'REDIS__HOST must be set if using the RedisQueue'
+        return data
+
+    @model_validator(mode='after')
+    @classmethod
+    def validate_output_settings(cls, data: Any) -> Any:
+        if isinstance(data, dict):
+            if data.get('output').rsplit('.')[-1] == 'dump_to_mongo':
+                assert data.get('mongo') is not None, 'MONGO__HOST must be set if using the dump_to_mongo output'
+        return data
+
+settings = Settings()
 
 # TODO: Use pydantic settings to pull out application settings, like redis host, port, etc
 async def main():
-    # Setup the redis queue and wait for the healthcheck
-    queue = RedisQueue('localhost')
-    await queue.healthcheck()
-    # AsyncIOMotorClient is an asynchronous mongodb client
-    mongoclient = AsyncIOMotorClient('localhost', 27017, username='root', password='example')
-    # Dummy function call to force the connection
-    _ = await mongoclient.list_database_names()
-    database: AsyncIOMotorDatabase = mongoclient['NFHM']
-    collection: AsyncIOMotorCollection = database['gbif']
+    worker_kwargs = {
+        'input_func': settings.input
+    }
+    work_kwargs = {
+        'source_queue': settings.source_queue
+    }
 
-    # Sample worker that'll have the "source" of gbif.  The source is just the queue that it'll be 
+    # Setup the redis queue and wait for the healthcheck
+    if settings.queue.__name__ == 'RedisQueue':
+        queue: RedisQueue = settings.queue(**settings.redis.model_dump())
+        # TODO define worker here
+    await queue.healthcheck()
+    worker_kwargs['queue'] = queue
+
+    if settings.mongo:
+        # AsyncIOMotorClient is an asynchronous mongodb client
+        mongoclient = AsyncIOMotorClient('localhost', 27017, username='root', password='example')
+        # Dummy function call to force the connection
+        _ = await mongoclient.list_database_names()
+        database: AsyncIOMotorDatabase = mongoclient['NFHM']
+        collection: AsyncIOMotorCollection = database['gbif']
+
+    if settings.output.__name__ == 'dump_to_mongo':
+        worker_kwargs['output_func'] = settings.output
+        worker_kwargs['output_kwargs'] = settings.source_queue
+
+    # Sample worker that'll have the "source_queue" of gbif.  The source_queue is just the queue that it'll be 
     # dequeuing from.  This worker also needs it's "input" and "output" functions defined
     # The "input" function is an awaitable function that takes in a URL.  In the future, I'd like
     # this to be more modular by allowing the developer to pass in kwargs like the output function
@@ -49,8 +109,8 @@ async def main():
 
     # Create the async tasks which is the work function for the worker.
     tasks = [
-        asyncio.create_task(worker.work(source))
-        for source, worker in workers
+        asyncio.create_task(worker.work(source_queue))
+        for source_queue, worker in workers
     ]
 
     # Gracefully handle shutting down here
