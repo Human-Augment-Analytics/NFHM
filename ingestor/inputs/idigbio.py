d85f91b5cbab6bd66f37ba2f56fb1f758f17491b
Shimer, Jacob Daniel <jacob.d.shimer@gmail.com>
2024-06-25 20:18:38 -0400

    - Adding n number of workers based off of CPU cores or off of user input - Queuing the next idigbio job before processing the current one

--- ingestor/inputs/idigbio.py
+++ ingestor/inputs/idigbio.py
@@ -42,6 +42,14 @@ async def idigbio_search(args: str, opts: dict) -> list[dict[Any, Any]]:
         if r.status_code == 200:
             try:
                 data = r.json()
+                if (import_all):
+                    # Push job for next page of results onto queue
+                    queue = opts['queue']
+                    offset = params.get('offset', 0) + pagesize
+                    next_job = { 'search_dict': search_dict, 'import_all': True, 'offset': offset }
+                    if (data['itemCount'] > offset):
+                        logger.info(f'Enqueuing next job with offset {offset} out of {data["itemCount"]} records.')
+                        await queue.enqueue('idigbio', json.dumps(next_job))
                 for record in data['items']:
                     logger.info(f'Parsing {record["uuid"]}')
                     record['media'] = []
@@ -54,14 +62,6 @@ async def idigbio_search(args: str, opts: dict) -> list[dict[Any, Any]]:
                                 logger.exception(f'Failed to get the media with UUID {uuid}, belonging to record {record["uuid"]}')
                         logger.info(f'Found media record for {uuid}')
                         record['media'].append(media_data)
-                if (import_all):
-                    # Push job for next page of results onto queue
-                    queue = opts['queue']
-                    offset = params.get('offset', 0) + pagesize
-                    next_job = { 'search_dict': search_dict, 'import_all': True, 'offset': offset }
-                    if (data['itemCount'] > offset):
-                        logger.info(f'Enqueuing next job with offset {offset} out of {data["itemCount"]} records.')
-                        await queue.enqueue('idigbio', json.dumps(next_job))
 
                 return data['items']
             except Exception:
