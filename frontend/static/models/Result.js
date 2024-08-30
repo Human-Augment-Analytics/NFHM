4d928ede102327389145d2614ee7c0d1d8875e8a
rdombrovski <romouald.dombrovski@gmail.com>
2024-08-30 10:34:13 -0400

    refactored frontend into components, vue/js modules

---
+++ frontend/static/models/Result.js
@@ -0,0 +1,26 @@
+export default class Result {
+    constructor(search_results) {
+      this.id = search_results.id;
+      this.media_url = search_results.media_url;
+      const url = new URL(this.media_url);
+      this.image_source_name = url.host;
+      this.latitude = search_results.latitude;
+      this.longitude = search_results.longitude;
+      this.map_url = `https://maps.google.com/?q=${this.latitude}%2C${this.longitude}`;
+      this.source_id = search_results.specimen_id;
+      this.source = search_results.source;
+  
+      this.name = search_results.scientific_name;
+      this.description = search_results.description;
+  
+      switch (this.source.toLowerCase()) {
+        case "idigbio":
+          this.source_link = `https://www.idigbio.org/portal/records/${search_results.specimen_id}`;
+          break;
+        default:
+          console.log("unidentified source " + search_results.source);
+          this.source_link = "#";
+      }
+    }
+  }
+  
