fe5e6df94769e95bb76f09701dcf3656641113ba
Thomas <deatheragetr@users.noreply.github.com>
2024-08-30 11:57:03 -0400

    Multiple improvemtns to the idigbio, vector embedder and inputs and postgres output function: 1) Improved and more comprehensive type hinting; Less noisy logs; better error handling; retries on failure (#28)

--- ingestor/ingestor.py
+++ ingestor/ingestor.py
@@ -5,7 +5,11 @@ import signal
 from typing import Any, Callable, Type
 
 
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase, AsyncIOMotorCollection
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorDatabase,
+    AsyncIOMotorCollection,
+)
 from pydantic import BaseModel, Field, ImportString, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -24,13 +28,12 @@ import asyncpg
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.StreamHandler()
-    ]
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
 )
 
 logger = logging.getLogger(__name__)
+
 
 class RedisSettings(BaseModel):
     host: str
@@ -39,50 +42,59 @@ class RedisSettings(BaseModel):
     username: str | None = None
     password: str | None = None
 
+
 class MongoSettings(BaseModel):
     host: str
     port: int = Field(default=27017)
-    database: str = Field(default='NFHM')
+    database: str = Field(default="NFHM")
     username: str | None = None
     password: str | None = None
-    input_collection: str = Field(default='idigbio')
+    input_collection: str = Field(default="idigbio")
+
 
 class PostgresSettings(BaseModel):
     host: str
     port: int = Field(default=5432)
-    database: str = Field(default='nfhm')
-    table: str = Field(default='search_records')
+    database: str = Field(default="nfhm")
+    table: str = Field(default="search_records")
     user: str | None = None
     password: str | None = None
 
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_nested_delimiter='__')
+    model_config = SettingsConfigDict(env_nested_delimiter="__")
     source_queue: str
     redis: RedisSettings | None = None
     mongo: MongoSettings | None = None
     # number_of_workers: int = Field(default=multiprocessing.cpu_count())
     number_of_workers: int = Field(default=1)
     postgres: PostgresSettings | None = None
-    queue: ImportString[Type[BaseQueue]] = Field(default='ingest_queue.RedisQueue')
-    input: ImportString[Callable[[Any], Any]] = Field(default='inputs.gbif_search')
-    output: ImportString[Callable[[Any], Any]] = Field(default='outputs.dump_to_mongo')
+    queue: ImportString[Type[BaseQueue]] = Field(default="ingest_queue.RedisQueue")
+    input: ImportString[Callable[[Any], Any]] = Field(default="inputs.gbif_search")
+    output: ImportString[Callable[[Any], Any]] = Field(default="outputs.dump_to_mongo")
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     @classmethod
     def validate_queue_settings(cls, data: Any) -> Any:
         if isinstance(data, dict):
-            if data.get('queue').rsplit('.')[-1] == 'RedisQueue':
-                assert data.get('redis') is not None, 'REDIS__HOST must be set if using the RedisQueue'
+            if data.get("queue").rsplit(".")[-1] == "RedisQueue":
+                assert (
+                    data.get("redis") is not None
+                ), "REDIS__HOST must be set if using the RedisQueue"
         return data
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     @classmethod
     def validate_output_settings(cls, data: Any) -> Any:
         if isinstance(data, dict):
-            if data.get('output').rsplit('.')[-1] == 'dump_to_mongo':
-                assert data.get('mongo') is not None, 'MONGO__HOST must be set if using the dump_to_mongo output'
-            if data.get('output').rsplit('.')[-1] == 'index_to_postgres':
-                assert data.get('postgres') is not None, 'POSTGRES_HOST must be set if using the index_to_postgres output'
+            if data.get("output").rsplit(".")[-1] == "dump_to_mongo":
+                assert (
+                    data.get("mongo") is not None
+                ), "MONGO__HOST must be set if using the dump_to_mongo output"
+            if data.get("output").rsplit(".")[-1] == "index_to_postgres":
+                assert (
+                    data.get("postgres") is not None
+                ), "POSTGRES_HOST must be set if using the index_to_postgres output"
         return data
 
 
@@ -92,27 +104,25 @@ settings = Settings()
 async def main():
     logger.info(settings.model_dump_json())
     worker_kwargs = {
-        'queue': None,
-        'input_func': settings.input,
-        'output_func': settings.output,
-        'output_kwargs': {},
-        'input_kwargs': {}
+        "queue": None,
+        "input_func": settings.input,
+        "output_func": settings.output,
+        "output_kwargs": {},
+        "input_kwargs": {},
     }
 
-    work_kwargs = {
-        'source_queue': settings.source_queue
-    }
+    work_kwargs = {"source_queue": settings.source_queue}
 
     # if settings.input.__name__ == 'vector_embedder':
     #     await load_model()
 
     worker_klass = None
     # Setup the redis queue and wait for the healthcheck
-    if settings.queue.__name__ == 'RedisQueue':
-        worker_kwargs['queue'] = settings.queue(**settings.redis.model_dump())
+    if settings.queue.__name__ == "RedisQueue":
+        worker_kwargs["queue"] = settings.queue(**settings.redis.model_dump())
         worker_klass = RedisWorker
-        worker_kwargs['input_kwargs']['queue'] = worker_kwargs['queue']
-    await worker_kwargs['queue'].healthcheck()
+        worker_kwargs["input_kwargs"]["queue"] = worker_kwargs["queue"]
+    await worker_kwargs["queue"].healthcheck()
 
     # We're using mongo
     if settings.mongo:
@@ -121,18 +131,20 @@ async def main():
             settings.mongo.host,
             settings.mongo.port,
             username=settings.mongo.username,
-            password=settings.mongo.password
+            password=settings.mongo.password,
         )
         # Dummy function call to force the connection
         _ = await mongoclient.list_database_names()
         database: AsyncIOMotorDatabase = mongoclient[settings.mongo.database]
         collection: AsyncIOMotorCollection = database[settings.source_queue]
         # Some input functions need access to Mongo client for input.
-        if worker_kwargs['output_func'].__name__ == 'dump_to_mongo':
-            worker_kwargs['output_kwargs']['collection'] = collection
+        if worker_kwargs["output_func"].__name__ == "dump_to_mongo":
+            worker_kwargs["output_kwargs"]["collection"] = collection
         if settings.mongo.input_collection:
-            worker_kwargs['input_kwargs']['mongo_database'] = database
-            worker_kwargs['input_kwargs']['mongo_collection'] = database[settings.mongo.input_collection]
+            worker_kwargs["input_kwargs"]["mongo_database"] = database
+            worker_kwargs["input_kwargs"]["mongo_collection"] = database[
+                settings.mongo.input_collection
+            ]
 
     if settings.postgres:
         conn = await asyncpg.connect(
@@ -140,18 +152,15 @@ async def main():
             port=settings.postgres.port,
             database=settings.postgres.database,
             user=settings.postgres.user,
-            password=settings.postgres.password
+            password=settings.postgres.password,
         )
         # Confirm connection
         _ = await conn.fetch("SELECT datname FROM pg_database")
-        
-        worker_kwargs['output_kwargs']['conn'] = conn
-        worker_kwargs['output_kwargs']['table'] = settings.postgres.table
 
-    
+        worker_kwargs["output_kwargs"]["conn"] = conn
+        worker_kwargs["output_kwargs"]["table"] = settings.postgres.table
+
     # D.I. queue for the input functions
-
-
 
     # Sample worker that'll have the "source_queue" of gbif.  The source_queue is just the queue that it'll be
     # dequeuing from.  This worker also needs it's "input" and "output" functions defined
@@ -166,17 +175,14 @@ async def main():
     ]
 
     # Create the async tasks which is the work function for the worker.
-    tasks = [
-        asyncio.create_task(worker.work(**work_kwargs))
-        for worker in workers
-    ]
+    tasks = [asyncio.create_task(worker.work(**work_kwargs)) for worker in workers]
 
     # Gracefully handle shutting down here
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
 
     def shutdown():
-        logger.info('Received stop signal, shutting down...')
+        logger.info("Received stop signal, shutting down...")
         stop.set_result(None)
 
     loop.add_signal_handler(signal.SIGINT, shutdown)
@@ -191,7 +197,8 @@ async def main():
 
     # Gather the tasks and close the redis connection
     await asyncio.gather(*tasks, return_exceptions=True)
-    await worker_kwargs['queue'].close()
+    await worker_kwargs["queue"].close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
--- ingestor/inputs/vector_embeddings.py
+++ ingestor/inputs/vector_embeddings.py
@@ -1,55 +1,56 @@
 import traceback
-import math
-import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
 import numpy as np
+from numpy.typing import NDArray
 import aiohttp
-import requests
-from PIL import Image
-from io import BytesIO
-from logging import getLogger
-from typing import Any
 import asyncio
 import json
 import torch
 import open_clip
-from datetime import datetime
-from bson.objectid import ObjectId
+from dateutil import parser as date_parser
+from PIL import Image
+from io import BytesIO
+import logging
+from typing import Any, List, Dict, Optional
+from schema.processed_search_record import ProcessedSearchRecord
 
-logger = getLogger('vector_embedder')
+logger = logging.getLogger("vector_embedder")
+logging.getLogger("PIL.TiffImagePlugin").setLevel(logging.INFO)
+logging.getLogger("pymongo.command").setLevel(logging.INFO)
+logging.getLogger("pymongo.serverSelection").setLevel(logging.INFO)
 
 model = None
 preprocess = None
 tokenizer = None
-device = None
+device = (
+    torch.device("mps:0") if torch.backends.mps.is_available() else torch.device("cpu")
+)
 
-if torch.backends.mps.is_available():
-    device = torch.device("mps:0")
-    logger.info("Using MPS")
-else:
-    device = torch.device("cpu")
+logger.info(f"Using device: {device}")
+
 
 async def load_model():
-    """
-    Loads the pre-trained model and tokenizer for vector embeddings.
-    This is lazily evaluated once when the first call to vector_embedder is made.
-    Loading the model is a slow operation that breaks ingestor.py when done at the top level upon import
-    """
-    global model, preprocess, tokenizer, device
-    # if model is not None:
-    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k', device=device)
-    model.to(device)
-    model.eval()  # model in train mode by default, impacts some models with BatchNorm or stochastic depth active
-    tokenizer = open_clip.get_tokenizer('ViT-B-32')
-    logger.info("Model loaded")
-    # else:
-        # logger.info("Model loaded already")
+    global model, preprocess, tokenizer
+    if model is None:
+        model, _, preprocess = open_clip.create_model_and_transforms(
+            "ViT-B-32", pretrained="laion2b_s34b_b79k", device=device
+        )
+        model.to(device)
+        model.eval()
+        tokenizer = open_clip.get_tokenizer("ViT-B-32")
+        logger.info("Model loaded")
+    else:
+        logger.info("Model already loaded")
 
-def input_data_mapper(media: dict, line: dict) -> dict:
-    tmp_d = {
-        "specimen_uuid": line.get("uuid"),
-        "scientific_name": line.get("data", {}).get("dwc:scientificName") or line.get("data", {}).get("dwc:genus"),
-        "external_media_uri": media.get("data", {}).get("ac:accessURI") or media.get("indexTerms", {}).get("accessuri"),
+
+def input_data_mapper(
+    media: Dict[str, Any], line: Dict[str, Any]
+) -> ProcessedSearchRecord:
+    record: ProcessedSearchRecord = {
+        "specimen_uuid": line.get("uuid", ""),
+        "scientific_name": line.get("data", {}).get("dwc:scientificName")
+        or line.get("data", {}).get("dwc:genus"),
+        "external_media_uri": media.get("data", {}).get("ac:accessURI")
+        or media.get("indexTerms", {}).get("accessuri"),
         "media_uuid": media.get("uuid"),
         "catalog_number": line.get("data", {}).get("dwc:catalogNumber"),
         "recorded_by": line.get("data", {}).get("dwc:recordedBy"),
@@ -61,30 +62,47 @@ def input_data_mapper(media: dict, line: dict) -> dict:
         "tax_genus": line.get("data", {}).get("dwc:genus"),
         "common_name": line.get("indexTerms", {}).get("commonname"),
         "higher_taxon": line.get("indexTerms", {}).get("highertaxon"),
-        "earliest_epoch_or_lowest_series": line.get("indexTerms", {}).get("earliestepochorlowestseries") or line.get("data", {}).get("dwc:earliestEpochOrLowestSeries"),
-        "earliest_age_or_lowest_stage": line.get("indexTerms", {}).get("earliestageorloweststage") or line.get("data", {}).get("dwc:dwc:earliestAgeOrLowestStage"),
-        "collection_date": line.get("indexTerms", {}).get("datecollected")
+        "earliest_epoch_or_lowest_series": line.get("indexTerms", {}).get(
+            "earliestepochorlowestseries"
+        )
+        or line.get("data", {}).get("dwc:earliestEpochOrLowestSeries"),
+        "earliest_age_or_lowest_stage": line.get("indexTerms", {}).get(
+            "earliestageorloweststage"
+        )
+        or line.get("data", {}).get("dwc:dwc:earliestAgeOrLowestStage"),
+        "collection_date": None,
+        "location": None,
+        "embedding": None,
+        "tensor_embedding": None,
     }
 
-    if line.get("indexTerms", {}).get("geopoint", {}).get("lon") is not None:
-        lat = str(line.get("indexTerms", {}).get("geopoint", {}).get("lat"))
-        lon = str(line.get("indexTerms", {}).get("geopoint", {}).get("lon"))
-        tmp_d["location"] = f"POINT({lon} {lat})"
+    geopoint = line.get("indexTerms", {}).get("geopoint", {})
+    if geopoint.get("lon") is not None and geopoint.get("lat") is not None:
+        record["location"] = f"POINT({geopoint['lon']} {geopoint['lat']})"
 
-    if tmp_d["collection_date"] is not None:
-        tmp_d["collection_date"] = datetime.strptime('2012-01-12', '%Y-%m-%d');
+    collection_date = line.get("indexTerms", {}).get("datecollected")
+    if collection_date is not None:
+        try:
+            parsed_date = date_parser.parse(collection_date)
+            record["collection_date"] = parsed_date.date()
+        except ValueError:
+            logger.warning(
+                f"Invalid date format for collection_date: {collection_date}"
+            )
 
-    return tmp_d
+    return record
 
 
-async def extract_data(collection: Any, page_size: int, page_offset: int):
+async def extract_data(
+    collection: Any, page_size: int, page_offset: int
+) -> List[ProcessedSearchRecord]:
     projection = {
         "uuid": 1,
         "media.uuid": 1,
         "media.data": 1,
         "media.indexTerms": 1,
         "media.ac:accessURI": 1,
-        "data.dwc:scientificName":  1,
+        "data.dwc:scientificName": 1,
         "data.dwc:genus": 1,
         "data.dwc:family": 1,
         "data.dwc:order": 1,
@@ -98,134 +116,113 @@ async def extract_data(collection: Any, page_size: int, page_offset: int):
         "indexTerms.geopoint": 1,
         "indexTerms.earliestepochorlowestseries": 1,
         "indexTerms.earliestageorloweststage": 1,
-        "indexTerms.datecollected": 1
+        "indexTerms.datecollected": 1,
     }
 
-
-
     query = {}
-    # TODO: Turn the following comment into a command line example.
-    # If you're trying to re-start a job that's already proceeded for some time, and want to avoid redundant work
-    # You can uncomment the below line and replace the ObjectId with the last _id you processed, which can be found by looking 
-    # at the speciemen_uuid of the last record in PG and then mapping that to what's in the mongo collection to get the Mongo document object id
-    # query = {"_id": {"$gt": ObjectId("66882305ffdc56ce50b64652") }}
-    # // For sake of efficency, make sure sort is on an indexed field, otherwise skip will become very slow
-    cursor = collection.find(query, projection).sort({ "_id": 1 }).skip(page_offset * page_size).limit(page_size)
-    new_lines = []
-    for document in await cursor.to_list(length=page_size):
-        for media in document['media']:
-            data = input_data_mapper(media, document)
-            new_lines.append(data)
-    
-    return new_lines
+    cursor = (
+        collection.find(query, projection)
+        .sort("_id", 1)
+        .skip(page_offset * page_size)
+        .limit(page_size)
+    )
 
-async def download_image_and_preprocess(entry: dict) -> torch.Tensor:
-    """This method downloads an image asynchronously and outputs its vector in memory"""
-    image_location = entry['external_media_uri']
+    documents = await cursor.to_list(length=page_size)
+    return [
+        input_data_mapper(media, document)
+        for document in documents
+        for media in document["media"]
+    ]
 
-    # import pprint
-    # pprint.pp(entry)
-    logger.info("Begin downloading image from {}".format(entry))
 
-    async with aiohttp.ClientSession() as session:
+async def download_image(
+    session: aiohttp.ClientSession, image_location: str
+) -> Optional[Image.Image]:
+    try:
         async with session.get(image_location, allow_redirects=True) as response:
-            try: 
-                if response.status < 400:
-                    content = await response.read()
-                    logger.debug(f"Downloaded image from {image_location}")
-                    img = Image.open(BytesIO(content))
-                    logger.debug(f"Opened image from {image_location}")
-                    image = preprocess(img).unsqueeze(0).to(device)
-                    logger.debug(f"Preprocessed image from {image_location}")
-                    vector = model.encode_image(image)
-                    vector = vector.cpu()
-                    logger.debug(f"Encoded image from {image_location}")
-                else:
-                    logger.error(f"Failed to download image from {image_location}. Status code: {response.status}")
-                    vector = None
-            except Exception as e:
-                logger.error(e)
-                logger.error(f"Error downloading image from {image_location}")
-                vector = None
-            entry['tensor_embedding'] = vector
-            return entry
+            if response.status < 400:
+                content = await response.read()
+                return Image.open(BytesIO(content))
+            else:
+                logger.error(
+                    f"Failed to download image from {image_location}. Status code: {response.status}"
+                )
+                return None
+    except Exception as e:
+        logger.error(f"Error downloading image from {image_location}: {e}")
+        return None
 
 
-async def vector_embedder(args: str, opts: dict) -> list[dict[Any, Any]]:
+async def process_image(
+    session: aiohttp.ClientSession, record: ProcessedSearchRecord
+) -> ProcessedSearchRecord:
+    image_location = record["external_media_uri"]
+    if image_location:
+        img = await download_image(session, image_location)
+        if img is not None:
+            image = preprocess(img).unsqueeze(0).to(device)
+            with torch.no_grad():
+                vector = model.encode_image(image)
+            vector = vector.cpu()
+            record["tensor_embedding"] = vector
+        else:
+            logger.error(f"No image downloaded for: {image_location}")
+    else:
+        logger.error("No external_media_uri provided in the record")
+
+    return record
+
+
+async def vector_embedder(
+    args: str, opts: Dict[str, Any]
+) -> List[ProcessedSearchRecord]:
     try:
         params = json.loads(args)
-        collection = opts['mongo_collection']
-        queue = opts['queue']
-        page_size = params.get('page_size', 100)
-        page_offset = params.get('page_offset', 0)
+        collection = opts["mongo_collection"]
+        queue = opts["queue"]
+        page_size = params.get("page_size", 100)
+        page_offset = params.get("page_offset", 0)
 
-        if model is None:
-            await load_model()
+        await load_model()
 
         mongo_records = await extract_data(collection, page_size, page_offset)
 
-        # Greater than or equal because we extract the nested media data from each record, so there's more possibly than page_size mongo_records
-        if len(mongo_records) >= page_offset:
-            next_job = { 'page_size': page_size, 'page_offset': page_offset + 1 }
-            await queue.enqueue('embedder', json.dumps(next_job))
+        if len(mongo_records) >= page_size:
+            next_job = {"page_size": page_size, "page_offset": page_offset + 1}
+            logger.info(f"Enqueuing next job with offset {page_offset + 1}")
+            await queue.enqueue("embedder", json.dumps(next_job))
 
-        batch_size=10
-        num_chunks = math.ceil(len(mongo_records)/batch_size)
+        batch_size = 10
+        results: List[ProcessedSearchRecord] = []
 
-        results = []
-        for chunk in np.array_split(mongo_records, num_chunks):
-            # MULTITHREADED
-            # with ThreadPoolExecutor(max_workers=3) as executor:
-            #     loop = asyncio.get_running_loop()
-            #     # Wrap download_image_and_preprocess with asyncio.run_coroutine_threadsafe
-            #     tasks = [loop.run_in_executor(executor, lambda entry=entry: asyncio.run_coroutine_threadsafe(download_image_and_preprocess(entry), loop).result()) for entry in chunk]
-            #     entries_with_embeddings = await asyncio.gather(*tasks)
-            #     # downloaded_images = list(executor.map(download_image_and_preprocess, chunk))
+        async with aiohttp.ClientSession() as session:
+            for i in range(0, len(mongo_records), batch_size):
+                chunk = mongo_records[i : i + batch_size]
+                tasks = [process_image(session, record) for record in chunk]
+                processed_records = await asyncio.gather(*tasks)
 
-            #     # downloaded_images = executor.map(download_image_and_preprocess, entry)
-            #     for _i, record in enumerate(entries_with_embeddings):
-            #         image = record['tensor_embedding']
-            #         try:
-            #             if image is not None and image.numel() > 0:
-            #                 row = torch.nn.functional.normalize(image)
-            #                 row = row.detach().numpy().tolist()[0]
-            #                 vector = json.dumps(row)
-
-            #                 record['embedding'] = vector
-            #                 results.append(record)
-            #             else:
-            #                 logger.error("{}: No vector for image".format(record['media_location']))
-            #         except Exception as e:
-            #             logger.error(e)
-
-
-            # SINGLE THREADED
-            tasks = [download_image_and_preprocess(entry) for entry in chunk]
-
-            entries_with_embeddings = await asyncio.gather(*tasks)
-            # downloaded_images = list(executor.map(download_image_and_preprocess, chunk))
-
-            # downloaded_images = executor.map(download_image_and_preprocess, entry)
-            for _i, record in enumerate(entries_with_embeddings):
-                image = record['tensor_embedding']
-                try:
-                    if image is not None and image.numel() > 0:
-                        image = image.cpu()
-                        row = torch.nn.functional.normalize(image)
-                        row = row.detach().numpy().tolist()[0]
-                        vector = json.dumps(row)
-
-                        record['embedding'] = vector
+                for record in processed_records:
+                    if (
+                        record["tensor_embedding"] is not None
+                        and record["tensor_embedding"].numel() > 0
+                    ):
+                        row: torch.Tensor = torch.nn.functional.normalize(
+                            record["tensor_embedding"]
+                        )
+                        numpy_row: NDArray[np.float32] = np.asarray(
+                            row.detach().cpu(), dtype=np.float32
+                        )
+                        record["embedding"] = json.dumps(numpy_row.tolist()[0])
                         results.append(record)
                     else:
-                        logger.error("{}: No vector for image".format(record['external_media_uri']))
-                except Exception as e:
-                    logger.error(e)
+                        logger.error(
+                            f"No vector for image: {record['external_media_uri']}"
+                        )
 
-
-        logger.info(f"Finished processing {len(results)}")
+        logger.info(f"Finished processing {len(results)} images")
         return results
+
     except Exception as e:
+        logger.error(f"Encountered an error in vector_embedder: {e}")
         traceback.print_exc()
-        logger.error(e)
-        logger.error("Encountered an error in vector_embedder")
+        return []
